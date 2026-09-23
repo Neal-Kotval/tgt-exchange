90e3340e4d09f9d059ce404cf658c9ff0d0aa35d
Neal Kotval <nkotval3@gatech.edu>
2026-09-23 12:56:38 -0400

    added order book header file

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include "order.hpp"
+#include "order_book.hpp"
+
+using namespace std;
 
 int main() {
-    std::cout << "test" << '\n';
+    Order test{ 10, Side::Buy, 11, 12};
+    cout << test.id << '\n';
+    if (test.side == Side::Buy) {
+        cout << "Buy\n";
+    } else {
+        cout << "Sell\n";
+    }
+    cout << test.price << '\n';
+    cout << test.quantity << '\n';
 }
---
+++ order_book.hpp
@@ -0,0 +1,13 @@
+#ifndef TGT_EXCHANGE_ORDER_BOOK_HPP
+#define TGT_EXCHANGE_ORDER_BOOK_HPP
+
+#include "order.hpp"
+
+class OrderBook {
+    public:
+
+    private:
+
+};
+
+#endif
